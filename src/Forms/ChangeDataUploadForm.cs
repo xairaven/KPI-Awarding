0d48ec21f83f602c95cb1a9de7d82cc071e81ae4
Karkushevskiy Vlad <karkuhkarkuh13@gmail.com>
2022-11-29 09:21:22 +0200

    (feat/karkuh) Added ChangeDataUploadForm

---
+++ src/Forms/ChangeDataUploadForm.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace Program.Forms
+{
+    public partial class ChangeDataUploadForm : Form
+    {
+        public ChangeDataUploadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void manuallyBut_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void backBut_MouseClick(object sender, MouseEventArgs e)
+        {
+            StartForm startForm = new StartForm();
+            startForm.Show();
+            Hide();
+        }
+    }
+}
