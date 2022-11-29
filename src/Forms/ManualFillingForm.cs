f4e3b93157d8153901e0b87e89117dbcd315e9d9
Karkushevskiy Vlad <karkuhkarkuh13@gmail.com>
2022-11-29 10:24:57 +0200

    (feat/karkuh) Added ManualFillingForm

---
+++ src/Forms/ManualFillingForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Program.Forms
+{
+    public partial class ManualFillingForm : Form
+    {
+        public ManualFillingForm()
+        {
+            InitializeComponent();
+
+          
+        }
+
+
+        private void backBut_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Якщо ви вийдете, ви втратите всі дані.\n Ви впевнені?", 
+                "", 
+                MessageBoxButtons.YesNo, 
+                MessageBoxIcon.Question, 
+                MessageBoxDefaultButton.Button2, 
+                MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.Yes)
+            {
+                ChangeDataUploadForm changeDataUploadForm = new ChangeDataUploadForm();
+                changeDataUploadForm.Show();
+                Hide();
+            }
+
+        }
+    }
+ 
+
+}
