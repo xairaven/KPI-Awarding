f28a3373f96a253800478391f0b91c138c9f0dc1
Karkushevskiy Vlad <karkuhkarkuh13@gmail.com>
2022-11-30 12:27:01 +0200

    (feat/karkuh) Uploading excel files in the ChangeDataUploadForm and Created MainForm

---
+++ src/Forms/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+
+namespace Program.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backBut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(165, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(781, 372);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // backBut
+            // 
+            this.backBut.BackColor = System.Drawing.Color.Lavender;
+            this.backBut.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.backBut.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.backBut.Location = new System.Drawing.Point(1004, 27);
+            this.backBut.Name = "backBut";
+            this.backBut.Size = new System.Drawing.Size(150, 50);
+            this.backBut.TabIndex = 4;
+            this.backBut.Text = "Назад";
+            this.backBut.UseVisualStyleBackColor = false;
+            this.backBut.MouseClick += new System.Windows.Forms.MouseEventHandler(this.backBut_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.backBut);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button backBut;
+
+        public System.Windows.Forms.DataGridView dataGridView1;
+
+        #endregion
+    }
+}
---
+++ src/Forms/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace Program.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void backBut_MouseClick(object sender, MouseEventArgs e)
+        {
+            var changeDataUploadForm = new ChangeDataUploadForm();
+            changeDataUploadForm.Show();
+            Hide();
+        }
+    }
+}
